262137946f5fdf426756a28314593f9b5a87d0b7
sailinux6 <sailinux6@gmail.com>
2018-11-21 14:25:56 -0800

    shell script

---
+++ updateami-script.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# This script will test the updates on packer ami image
+
+# create temporary ec2 instance using packer ami image
+terraform init ./terraform
+terraform apply -auto-approve ./terraform
+
+sleep 60
+
+hostip=$(terraform output ec2_public_ip)
+amiid=$(terraform output packer_ami_id)
+user=centos
+
+# check for the updates on packer ami
+ssh -i ./key/tmpkey -o StrictHostKeyChecking=no $user@$hostip "sudo yum check-update" > /dev/null
+
+# set the variable if updates found
+if [ $? -eq 0 ]
+  then
+    echo "No updates found"
+	export updateami=no
+        updateami=no
+  else
+    echo "Update found on image"
+	export updateami=yes
+         updateami=yes
+fi
+
+# destroy the ec2 instance
+terraform destroy -auto-approve ./terraform
+sleep 10
+
+# Trigger another packer job from here to create ami
+
+if [ updateami == 'yes' ]
+  then
+    echo "updating packer image"
+	export amiid=$amiid
+	packer build ./packer/packer.json
+	
+	sleep 15
+    # destroying old ami
+    aws ec2 deregister-image --image-id $amiid
+fi
+
+echo "new ami updated successfully..."
+
+
