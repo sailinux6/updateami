35fa07fc8b9e4a87424a06c3d6625a393f27ff7e
sailinux6 <sailinux6@gmail.com>
2018-11-21 21:13:43 -0800

    fourth update

--- updateami-script.sh
+++ updateami-script.sh
@@ -20,15 +20,16 @@ ssh -i ./key/tmpkey -o StrictHostKeyChecking=no $user@$hostip "sudo yum check-up
 if [ $? -eq 0 ]
   then
     echo "No updates found"
-	export updateami=no
+    export updateami=no
     updateami=no
   else
     echo "Update found on image"
-	export updateami=yes
+    export updateami=yes
     updateami=yes
 fi
 
 # destroy the ec2 instance
+echo "destroying testami ec2 instance..."
 terraform destroy -auto-approve ./terraform
 sleep 10
 
@@ -36,15 +37,22 @@ sleep 10
 
 if [ $updateami == 'yes' ]
   then
-    echo "updating packer image"
-	export amiid=$amiid
-	packer build ./packer/packer.json
+    echo "updating packer image..."
+    export amiid=$amiid
+    packer build ./packer/packer.json
 	
-	sleep 15
-    # destroying old ami
-    aws ec2 deregister-image --image-id $amiid
+    
+    if [ $? -eq 0 ]
+      then
+	    echo "new ami updated successfully"
+		echo "destroying previous ami image..."
+        sleep 15
+        # destroying old ami
+        aws ec2 deregister-image --image-id $amiid
+		echo "$amiid destroyed successfully"
+    fi
 fi
 
-echo "new ami updated successfully..."
+
 
 
